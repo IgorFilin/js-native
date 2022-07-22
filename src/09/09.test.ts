605c635d94af8740f36183105599c48983816df8
Igor <filinigor@yandex.ru>
2022-07-22 18:07:13 +0300

    create 9 lesson

--- src/09/09.test.ts
+++ src/09/09.test.ts
@@ -7,3 +7,31 @@ test('Test age users',()=>{
 
    expect(user.age).toBe(29)
 })
+test('Test mutantObj',()=>{
+
+      let user2 = user
+       user2.age = 30
+
+
+    expect(user.age).toBe(30)
+    expect(user).toEqual(user2)
+})
+test('Test mutantArray',()=>{
+
+      let arrayUsers = [
+          {
+              name:'Igor',
+              age:28
+          },
+          {
+              name:'Vadim',
+              age:32
+          }
+      ]
+       let array2 = arrayUsers
+       array2[0].name = 'Igoryan'
+
+
+    expect(arrayUsers[0].name).toBe('Igoryan')
+    expect(arrayUsers).toEqual(array2)
+})
